02c6fa42a9269604c33e514a3e4e6c3a81f67ac3
SebastianFredes <sebastian.fredes1601@alumnos.ubiobio.cl>
2019-10-10 17:47:06 -0400

    Initial commit

---
+++ app/src/main/java/cl/ubb/app2/MainActivity.java
@@ -0,0 +1,14 @@
+package cl.ubb.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
